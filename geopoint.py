9ce5d1a7190245d57889dfc298fa011878f6cd5b
bigb123 <piotr.palka123@gmail.com>
2016-01-21 23:05:48 +0100

    Before tidy changes

---
+++ geopoint.py
@@ -0,0 +1,12 @@
+class Geopoint:
+    id_number = 0
+    lat = 0.0
+    lon = 0.0
+    r = 0.0
+
+
+    def __init__(self, id_number, lat, lon, r):
+        self.id_number = id_number
+        self.lat = lat
+        self.lon = lon
+        self.r = r
---
+++ main.py
@@ -0,0 +1,209 @@
+from math import sqrt, pow
+import random
+from time import time
+import psycopg2
+
+from geopoint import Geopoint
+
+""" Max avaliable values """
+LAT_MAX = 90
+LON_MAX = 180
+R_MAX = 40000   # maximum radius is size of equator in kilometers
+
+POINT_LIST = []
+POINT_LIST_CLASS = []
+
+
+def distance(lat1, lon1, lat2, lon2):
+    return sqrt(
+            pow(lat1 - lat2, 2) + pow(lon1 - lon2, 2)
+    )
+
+
+def value_generator(name):
+    multi_and_div = 1000000     # used to obtain 6-decimal-places float values
+
+    if 'lat' == name:
+        return random.randrange(-LAT_MAX * multi_and_div, LAT_MAX * multi_and_div) / multi_and_div
+    if 'lon' == name:
+        return random.randrange(-LON_MAX * multi_and_div, LON_MAX * multi_and_div) / multi_and_div
+    if 'r' == name:
+        return random.randrange(0, R_MAX * (multi_and_div/100)) / multi_and_div
+
+
+def generator(number_of_points):
+    point_list = []
+
+    for i in range(number_of_points):
+        point_list.append([i, value_generator('lat'), value_generator('lon'), value_generator('r')])
+
+    return point_list
+
+
+def batch_create(point_list):
+    point_list_class = []
+    for one_point in point_list:
+        point_list_class.append(Geopoint(one_point[0], one_point[1], one_point[2], one_point[3]))
+
+    return point_list_class
+
+
+def list_search(lat, lon, point_list):
+    print('List-based searching:')
+    id_list = []
+
+    curtime = time()
+    for point in point_list:
+        if distance(lat, lon, point[1], point[2]) <= point[3]:
+            id_list.append(point[0])
+
+    final_time = time()-curtime
+
+    return id_list, final_time
+
+
+def class_search(lat, lon, point_list):
+    print('\nClass-based searching:')
+    id_list = []
+
+    """ Insert into class """
+    point_list_class = batch_create(point_list)
+
+    """ Search """
+    curtime = time()
+    for point in point_list_class:
+        if distance(lat, lon, point.lat, point.lon) <= point.r:
+            id_list.append(point.id_number)
+
+    final_time = time()-curtime
+
+    return id_list, final_time
+
+
+# def dict_search(lat, lon, point_list):
+#
+#
+# def database_search(lat, lon, point_list):
+
+
+
+def query(lat, lon):
+    no_of_records = 100
+
+    if lat > LAT_MAX or lat < -LAT_MAX:
+        print('Latiture value must be between', -LAT_MAX, 'and', LAT_MAX, '. Your value:', lat)
+        return
+
+    if lon > LON_MAX or lon < -LON_MAX:
+        print('Longitude value must be between', -LON_MAX, 'and', LON_MAX, '. Your value:', lon)
+        return
+
+    point_list = generator(no_of_records)
+
+
+    """ List-based searching """
+    search_name =  'list'
+    id_list, final_time = list_search(lat, lon, point_list)
+    time_dict = {search_name : final_time}
+    index_dict = {search_name : id_list}
+
+
+    """ Class-based searching """
+    search_name =  'class'
+    id_list, final_time = class_search(lat, lon, point_list)
+    time_dict[search_name] = final_time
+    index_dict[search_name] =  id_list
+
+    # counter = 0
+    # print('\nClass-based searching:')
+    # id_list_class = []
+    #
+    # """ Insert into class """
+    # point_list_class = batch_create(point_list)
+    #
+    # """ Search """
+    # curtime = time()
+    # for point in point_list_class:
+    #     if distance(lat, lon, point.lat, point.lon) <= point.r:
+    #         id_list_class.append(point.id_number)
+    #         counter += 1
+    #
+    # time_dict['class'] = time()-curtime
+    # print('Amount of points:', counter)
+
+
+    """ Dictionary searching """
+    counter = 0
+    print('\nDictionary searching:')
+    id_list_dict = []
+
+    """ Insert into dictionary"""
+    list_dict = []
+    for point in point_list:
+        temp_dict = {'id' : point[0], 'lat' : point[1], 'lon' : point[2], 'r' : point[3]}
+        list_dict.append(temp_dict)
+
+    """ Search """
+    curtime = time()
+    for point in list_dict:
+        if distance(lat, lon, point['lat'], point['lon']) <= point['r']:
+            counter += 1
+            id_list_dict.append(point['id'])
+
+    time_dict['dict'] = time()-curtime
+    print('Amount of points:', counter)
+
+
+
+    """ Database searching """
+    print('\nDatabase searching:')
+
+    try:
+        conn = psycopg2.connect('host=192.168.0.16 user=pgwojtek dbname=cctest password=cctestpasswd0%0')
+    except:
+        print('Cannot connect to database')
+        exit(1)
+
+    cur = conn.cursor()
+
+    """ Insert into database """
+    cur.execute('drop table if exists points cascade;')
+    conn.commit()
+
+    cur.execute("""create table points (
+                    id serial primary key,
+                    lat float,
+                    lon float,
+                    r float
+                );"""
+                )
+    conn.commit()
+
+    for point in point_list:
+        cur.execute("""insert into points (lat, lon, r) values(%s, %s, %s)""", (point[1], point[2], point[3]))
+    conn.commit()
+
+    """ Search """
+    curtime = time()
+    cur.execute("""select id from points where |/((%s - lat)^2 + (%s - lon)^2) <= r;""", (lat, lon))
+    conn.commit()
+    rows = cur.fetchall()
+
+    time_dict['database'] = time()-curtime
+
+    cur.close()
+    conn.close()
+
+    print(len(rows))
+
+    print('Times\n', time_dict)
+    print('Indexes\n', index_dict)
+
+
+def main():
+
+    query(58.435287, 100)
+
+
+if __name__ == '__main__':
+    main()
